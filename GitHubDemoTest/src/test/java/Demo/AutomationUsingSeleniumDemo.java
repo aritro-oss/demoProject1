3e0b1dbb3d0bedde56ad26f43d2a8ad65235fe1b
Aritro <Aritro@LAPTOP-JM0703NE>
2025-09-24 23:29:45 +0530

    using e

--- GitHubDemoTest/src/test/java/Demo/AutomationUsingSeleniumDemo.java
+++ GitHubDemoTest/src/test/java/Demo/AutomationUsingSeleniumDemo.java
@@ -3,24 +3,6 @@ package Demo;
 public class AutomationUsingSeleniumDemo {
 
 	public static void main(String[] args) {
-		    public WebDriver driver;
-
-		    @BeforeMethod
-		    public void launchDriver(){
-		        driver = new ChromeDriver();
-		        driver.manage().window().maximize();
-		    }
-
-		    @Test
-		    public void test1(){
-		        driver.navigate().to("https://www.google.com/");
-		        System.out.println(driver.getTitle());
-		    }
-
-		    @AfterMethod
-		    public void closeBrowser(){
-		        driver.close();
+		    //pushing to github
 		    }
 		}
-
-}
